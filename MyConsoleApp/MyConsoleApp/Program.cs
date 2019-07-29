1b0e3320c1e091eecda912cbbf64e8f9ee1af52d
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-07-29 15:38:40 -0400

    Add basic dotnetcore + vue apps

---
+++ MyConsoleApp/MyConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Enter to Exit");
+            Console.ReadLine();
+        }
+    }
+}
