bf43908c581e62f6c39d2366a00e832bebd82d4d
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-07-30 10:10:51 -0400

    trying to set up dynamic version updates

---
+++ appveyor-set_version.ps1
@@ -0,0 +1,15 @@
+Write-Output "Checking Tag Version"
+if ($env:APPVEYOR_REPO_TAG -eq $true) {
+  Write-Output "Build triggered from tag, checking for SemVer naming"
+  $regex = new-object System.Text.RegularExpressions.Regex ('v(?<version>(?<major>[0-9]+).(?<minor>[0-9]+).(?<patch>[0-9]+)-?(?<prerelease>.+)?)', [System.Text.RegularExpressions.RegexOptions]::MultiLine)
+  $version = $null
+  $match = $regex.Match($env:APPVEYOR_REPO_TAG_NAME)
+  if($match.Success) {
+    Write-Output "SemVer naming found"
+    $version = $match.Groups["version"].Value + "+build.{build}"
+    Write-Output "Changing version '$env:APPVEYOR_BUILD_VERSION' to '$version'"
+    Update-AppveyorBuild -Version "$version"
+  }
+} else {
+  Write-Output "Build not triggered from tag, no version change necessary"
+}
