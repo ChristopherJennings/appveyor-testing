d8a510aa019102f56f18ddc9461e4771a6ad5c70
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-07-30 11:16:03 -0400

    Switch package script to ps

--- appveyor-before_package.cmd
+++
@@ -1,7 +0,0 @@
-dotnet publish .\MyConsoleApp\MyConsoleApp.csproj /p:PublishDir=publish -c Release -r win-x64 --self-contained true
-
-xcopy my-vue-app\dist publish\dist /E /I
-
-7z a appveyor_test-%APPVEYOR_BUILD_VERSION%.zip publish\*
-
-appveyor PushArtifact appveyor_test-%APPVEYOR_BUILD_VERSION%.zip
