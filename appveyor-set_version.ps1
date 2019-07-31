709a30d0ceb8c8d85d88d0d002d1658728545256
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-07-31 08:50:00 -0400

    Enhancements to set version

--- appveyor-set_version.ps1
+++ appveyor-set_version.ps1
@@ -1,5 +1,6 @@
 Write-Output "Checking Tag Version"
 $env:CurrentVersion = $env:APPVEYOR_BUILD_VERSION
+$env:SEMVER_VERSION = $env:APPVEYOR_BUILD_VERSION
 $tagName = & git describe --abbrev=0 --tags
 
 if ($env:APPVEYOR_REPO_TAG -eq $true) {
@@ -7,16 +8,22 @@ if ($env:APPVEYOR_REPO_TAG -eq $true) {
 }
 
 Write-Output "Checking tag for SemVer naming"
-$regex = new-object System.Text.RegularExpressions.Regex ('v(?<version>(?<major>[0-9]+).(?<minor>[0-9]+).(?<patch>[0-9]+)-?(?<prerelease>.+)?)', [System.Text.RegularExpressions.RegexOptions]::MultiLine)
+$regex = new-object System.Text.RegularExpressions.Regex ('v(?<versionwithprerelease>(?<version>(?<major>[0-9]+).(?<minor>[0-9]+).(?<patch>[0-9]+))(-(?<prerelease>.+))?)', [System.Text.RegularExpressions.RegexOptions]::MultiLine)
 $match = $regex.Match($tagName)
 if($match.Success) {
-  Write-Output "SemVer naming found"
+  Write-Output "SemVer naming found: $tagName"
   $version = $match.Groups["version"].Value
+  $versionwithprerelease = $match.Groups["versionwithprerelease"].Value
 
-  if ($env:APPVEYOR_REPO_TAG -ne $true) {
-    $version = $version + "+devbuild.$env:APPVEYOR_BUILD_NUMBER"
+  $dotNetVersion = "$version.$env:APPVEYOR_BUILD_NUMBER"
+
+  if ($env:APPVEYOR_REPO_TAG -eq $true) {
+    $env:SEMVER_VERSION = $versionwithprerelease
+  } else {
+    $env:SEMVER_VERSION = "$versionwithprerelease+devbuild.$env:APPVEYOR_BUILD_NUMBER"
   }
 
-  Write-Output "Changing version '$env:APPVEYOR_BUILD_VERSION' to '$version'"
-  Update-AppveyorBuild -Version "$version"
+  Write-Output "Changing version '$env:APPVEYOR_BUILD_VERSION' to '$dotNetVersion' based on tag"
+  Update-AppveyorBuild -Version "$dotNetVersion"
+  Write-Output "SemVer is: '$env:SEMVER_VERSION'"
 }
